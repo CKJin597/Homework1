77fde0ec44f970ce9e97959b50dec39d7e326c37
User <User@DESKTOP-RAE7E6C>
2024-01-30 08:58:50 +0800

    加入專案檔案。

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MSIT155.Models;
+
+namespace MSIT155.Controllers
+{
+    public class ApiController : Controller
+    {
+        private readonly MyDBContext _context;
+        public ApiController(MyDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cities()
+        {
+            var cities = _context.Addresses.Select(a => a.City).Distinct();
+            return Json(cities);
+        }
+
+    }
+}
