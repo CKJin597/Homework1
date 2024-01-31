0c7f07df49d46762b50385cdc88d59a017c38429
DESKTOP-RAE7E6C\User <kent81235@gmail.com>
2024-01-31 10:23:57 +0800

    01311023

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MSIT155.Models;
+using MSIT155Site.Models.DTO;
+using System.Text;
 
 namespace MSIT155.Controllers
 {
@@ -11,15 +13,47 @@ namespace MSIT155.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
         public IActionResult Cities()
         {
             var cities = _context.Addresses.Select(a => a.City).Distinct();
             return Json(cities);
+        }
+        public IActionResult Address() 
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            Thread.Sleep(3000);
+
+            //int x = 10;
+            //int y = 0;
+            //int z = x / y;
+            return Content("Content 你好!!", "text/plain", Encoding.UTF8);
+        }
+
+        //public IActionResult Register(string name, int age = 28)
+        public IActionResult Register(UserDTO _user)
+        {
+            if (string.IsNullOrEmpty(_user.Name))
+            {
+                _user.Name = "guest";
+            }
+            return Content($"Hello {_user.Name}, {_user.Age}歲了, 電子郵件是 {_user.Email}", "text/plain", Encoding.UTF8);
+        }
+        public IActionResult Avatar(int id = 1)
+        {
+            Member? member = _context.Members.Find(id);
+            if (member != null)
+            {
+                byte[] img = member.FileData;
+                if (img != null)
+                {
+                    return File(img, "image/jpeg");
+                }
+            }
+
+            return NotFound();
         }
 
     }
